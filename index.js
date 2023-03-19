6d876d2b768e1307859fbafc561ca73da3055541
Margaritkins <te120883ama@gmail.com>
2023-03-19 14:05:07 +0100

    task with function takes array

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+///task with function takes array
 const subscribers = [
   {firstName: 'qwert', lastName:'qwert', age:1},
   {firstName: 'qwert', lastName:'qwert', age:1},
@@ -17,10 +19,11 @@ const subscribers = [
   {firstName: 'qwert', lastName:'qwert', age:1}
 ]
 function congratsIfMoreThan100(){
-if (subscribers.length >= 20){
+if (subscribers.length >= 10){
   return alert('Congrats!');
 } else{
   return alert('Less than you expected')
 }
 }
 let showCongrats = congratsIfMoreThan100();
+
